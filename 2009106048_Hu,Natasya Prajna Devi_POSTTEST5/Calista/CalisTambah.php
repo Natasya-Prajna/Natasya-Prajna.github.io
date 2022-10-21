5034b72b3487865d10964fc9ad1e8b46dd454623
Natasya-Prajna <natasya.prajna@gmail.com>
2022-10-21 22:21:48 +0800

    Tugas Natasya

---
+++ 2009106048_Hu,Natasya Prajna Devi_POSTTEST5/Calista/CalisTambah.php
@@ -0,0 +1,19 @@
+<?php 
+
+require 'config.php';
+
+if(isset($_POST['submit'])){
+    $nomor = $_POST['noProduk'];
+    $nama = $_POST['namaProduk'];
+    $jumlah = $_POST['jumlahProduk'];
+    $harga = $_POST['hargaProduk'];
+
+    $kirim = mysqli_query($db, "INSERT INTO prodak (nomor_produk,nama_produk,jumlah_produk,harga_produk) VALUES ('$nomor','$nama','$jumlah','$harga')");
+
+    if($kirim){
+        // echo "<script> alert('Data Berhasil Dikirim');</script>";
+        header("Location:CalisTampil.php");
+    }else {
+        echo "gagal mengirim";
+    }
+}
